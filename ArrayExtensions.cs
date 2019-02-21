719e155c67300009f9c867f6f0a5209badb01b64
AAleksanderr <indra1@tut.by>
2019-02-21 13:07:01 +0300

    done

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace PadawansTask11
 {
@@ -6,8 +7,23 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (array == null) throw new ArgumentNullException();
+            if (array.Length < 3) throw new ArgumentException();
+            if (accuracy < 0 || accuracy > 1) throw new ArgumentException();
+            double[] leftsum = new double[array.Length];
+            double[] rightsum = new double[array.Length];
+            leftsum[0] = array[0];
+            rightsum[array.Length - 1] = array[array.Length - 1];
+            for (int i = 1; i < array.Length; i++)
+            {
+                leftsum[i] = leftsum[i - 1] + array[i];
+                rightsum[array.Length - i - 1] = rightsum[array.Length - i] + array[array.Length - i -1];
+            }
+
+            for (int i = 1; i < leftsum.Length - 1; i++)
+                if (Math.Abs(leftsum[i - 1] - rightsum[i + 1]) <= accuracy)
+                    return i;
+            return null;
         }
     }
 }
